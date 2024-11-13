4724c8422a6a9221eeb0bb3655724e19966c3cc1
xdocka15 <tomasdockl@seznam.cz>
2024-11-13 10:46:24 +0100

    MKS8

---
+++ NXP/lpcxpresso55s69_2_hello_world/source/hello_world.c
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017, 2024 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "peripherals.h"
+
+#include "fsl_power.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Main function
+ */
+void TimerIRQ(uint32_t peripherals){
+	PRINTF("hello world.ISR\r\n");
+}
+
+int main(void)
+{
+    char ch;
+
+    /* Init board hardware. */
+    /* set BOD VBAT level to 1.65V */
+    POWER_SetBodVbatLevel(kPOWER_BodVbatLevel1650mv, kPOWER_BodHystLevel50mv, false);
+    /* attach main clock divide to FLEXCOMM0 (debug console) */
+    CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
+
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitPeripherals();
+    BOARD_InitDebugConsole();
+#if !defined(DONT_ENABLE_FLASH_PREFETCH)
+    /* enable flash prefetch for better performance */
+    SYSCON->FMCCR |= SYSCON_FMCCR_PREFEN_MASK;
+#endif
+    CTIMER_StartTimer(CTIMER1_PERIPHERAL);
+    PRINTF("hello world.\r\n");
+
+    while (1)
+    {
+        ch = GETCHAR();
+        PUTCHAR(ch);
+    }
+}
